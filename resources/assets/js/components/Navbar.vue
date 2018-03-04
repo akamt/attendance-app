7bee37cde065ec131bf25aa8acb98d85c97dd4fd
Norio Kaneko <akamt68@gmail.com>
2018-03-04 22:16:29 +0900

    Navigation bar のrouterを有効化

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -1,20 +1,24 @@
 <template>
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item @click="toTop" index="1">
+    <el-menu :router="true" :default-active="link" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item @click="toTop" index="/">
             TOP
         </el-menu-item>
-        <el-submenu v-if="userStatus.authenticated" @click="toExpense" index="2">
+        <el-submenu v-if="userStatus.authenticated" index="/expense">
             <template slot="title">経費</template>
-            <el-menu-item @click="toExpense" index="2-1">経費登録</el-menu-item>
-            <el-menu-item @click="toExpenseList" index="2-2">経費一覧</el-menu-item>
+            <el-menu-item @click="toExpense" index="/expense">
+                経費登録
+            </el-menu-item>
+            <el-menu-item @click="toExpenseList" index="/list">
+                経費一覧
+            </el-menu-item>
         </el-submenu>
-        <el-menu-item v-if="userStatus.authenticated" @click="toCategory" index="3">
+        <el-menu-item v-if="userStatus.authenticated" @click="toCategory" index="/category">
             カテゴリー
         </el-menu-item>
-        <el-menu-item v-if="!userStatus.authenticated" @click="toLogin" index="4">
+        <el-menu-item v-if="!userStatus.authenticated" @click="toLogin" index="/login">
             Login
         </el-menu-item>
-        <el-menu-item v-if="userStatus.authenticated" @click="logout" index="4">
+        <el-menu-item v-if="userStatus.authenticated" @click="logout" index="/logout">
             Logout
         </el-menu-item>
     </el-menu>
@@ -22,12 +26,19 @@
 
 <script>
     import userStore from '../stores/userStore'
+    import http from '../services/http'
 
     export default {
         data() {
             return {
                 activeIndex: '1',
                 userStatus: userStore.state
+            }
+        },
+        computed: {
+            link() {
+                // TODO nav store
+                return this.$route.path;
             }
         },
         methods: {
@@ -52,7 +63,6 @@
             logout() {
                 userStore.logout(() => {
                     this.$router.push('/login');
-                    this.handleSelect('4', null);
                 })
             }
         }
