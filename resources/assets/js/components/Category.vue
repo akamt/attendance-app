eafe8a42c568ded1d130ae7a7e23f9d186a963b3
Norio Kaneko <akamt68@gmail.com>
2018-03-04 22:16:56 +0900

    el-meinを使用した形に

--- resources/assets/js/components/Category.vue
+++ resources/assets/js/components/Category.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
-        <el-input placeholder="経費項目名" v-model="name" clearable>
-        </el-input>
-        <el-button type="primary" @click="add">Add</el-button>
-    </div>
+    <el-main>
+        <el-form @keyup.enter.native="login" ref="form" :model="form" label-width="100px">
+            <el-form-item label="項目名">
+                <el-input placeholder="経費項目名" v-model="form.name" clearable>
+                </el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="add">Add</el-button>
+            </el-form-item>
+        </el-form>
+    </el-main>
 </template>
 
 <script>
@@ -12,7 +18,9 @@
     export default {
         data() {
             return {
-                name: ''
+                form:{
+                    name:''
+                }
             }
         },
         methods: {
