19db9214ee50616a12e60f8454def4bfb27984fd
Norio Kaneko <akamt68@gmail.com>
2018-03-04 22:19:14 +0900

    経費一覧画面で対象月を選択できるように

--- resources/assets/js/components/ExpenseList.vue
+++ resources/assets/js/components/ExpenseList.vue
@@ -1,5 +1,15 @@
 <template>
     <el-main>
+        <div class="selector-wrapper">
+            <el-select v-model="period" @change="periodChange" clearable placeholder="対象月を選択">
+                <el-option
+                        v-for="item in periodList"
+                        :key="item.month"
+                        :label="item.month"
+                        :value="item.month">
+                </el-option>
+            </el-select>
+        </div>
         <el-table :data="tableData" height="250" style="width: 100%">
             <el-table-column
                     prop="created_at"
@@ -12,13 +22,17 @@
                     width="180">
             </el-table-column>
             <el-table-column
-                    prop="kind"
-                    label="経費項目"
-                    width="180">
+                    prop="payment"
+                    label="支払先">
             </el-table-column>
             <el-table-column
                     prop="value"
                     label="値段">
+            </el-table-column>
+            <el-table-column
+                    prop="category_name"
+                    label="経費項目"
+                    width="180">
             </el-table-column>
         </el-table>
     </el-main>
@@ -30,13 +44,37 @@
     export default {
         data() {
             return {
+                periodList: [],
+                period: '',
                 tableData: []
             }
         },
         created() {
-            http.get('expenses', res => {
-                this.tableData = res.data;
-            })
+            http.get('period/list', res => {
+                this.periodList = res.data
+            });
+        },
+        methods: {
+            periodChange() {
+                let data = {
+                    period: this.period
+                };
+
+                http.post('expense/list', data, res => {
+                    this.tableData = res.data;
+                }, error => {
+                    console.log(error);
+                })
+            }
         }
     }
 </script>
+
+<style type="scss">
+    .selector-wrapper {
+        position: absolute;
+        right: 20px;
+        top: 10px;
+        width: 220px;
+    }
+</style>
