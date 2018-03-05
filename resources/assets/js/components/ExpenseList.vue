f06bbb5a7fae6f690e47846b54a5cd55f2cbf864
Norio Kaneko <akamt68@gmail.com>
2018-03-05 20:37:23 +0900

    経費一覧でのloading・日付項目・操作列を追加

--- resources/assets/js/components/ExpenseList.vue
+++ resources/assets/js/components/ExpenseList.vue
@@ -10,10 +10,11 @@
                 </el-option>
             </el-select>
         </div>
-        <el-table :data="tableData" height="250" style="width: 100%">
+        <el-table v-loading="loading" :data="tableData" height="100%" style="width: 100%">
             <el-table-column
-                    prop="created_at"
+                    prop="use_day"
                     label="日付"
+                    sortable
                     width="180">
             </el-table-column>
             <el-table-column
@@ -34,6 +35,15 @@
                     label="経費項目"
                     width="180">
             </el-table-column>
+            <el-table-column
+                    fixed="right"
+                    label="操作"
+                    width="120">
+                <template slot-scope="scope">
+                    <el-button @click="editExpense" type="text" size="small">Edit</el-button>
+                    <el-button @click="deleteExpense" type="text" size="small">Delete</el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </el-main>
 </template>
@@ -46,7 +56,8 @@
             return {
                 periodList: [],
                 period: '',
-                tableData: []
+                tableData: [],
+                loading: false
             }
         },
         created() {
@@ -60,11 +71,19 @@
                     period: this.period
                 };
 
+                this.loading = true;
                 http.post('expense/list', data, res => {
                     this.tableData = res.data;
+                    this.loading = false;
                 }, error => {
                     console.log(error);
                 })
+            },
+            editExpense() {
+                // TODO call API
+            },
+            deleteExpense() {
+                // TODO call API
             }
         }
     }
