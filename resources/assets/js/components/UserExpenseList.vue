06526d1b237f2e774ad224e26df383d3804efcba
Norio Kaneko <akamt68@gmail.com>
2018-03-15 00:41:53 +0900

    部下の経費一覧を表示

---
+++ resources/assets/js/components/UserExpenseList.vue
@@ -0,0 +1,17 @@
+<template>
+    <ExpenseList :user_id="$route.params.id"></ExpenseList>
+</template>
+
+<script>
+    import ExpenseList from './expense/List'
+
+    export default {
+        data() {
+            return {}
+        },
+        components: {
+            ExpenseList
+        },
+        methods: {}
+    }
+</script>
