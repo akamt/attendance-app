b4e0b4afc2fed8a7d5e2869167c0d64101e1abd8
Norio Kaneko <akamt68@gmail.com>
2018-03-15 00:39:24 +0900

    部下一覧のcomponentを実装

---
+++ resources/assets/js/components/UserList.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-main>
+        <el-table v-loading="listLoading" class="tb-edit" :data="tableData" highlight-current-row
+                  style="width:100%;">
+            <!--<span slot="empty">-->
+            <!--対象月を選択-->
+            <!--</span>-->
+            <el-table-column
+                    prop="name"
+                    label="名前"
+                    width="180">
+                <template slot-scope="scope">
+                    <span @click="toList(scope.row.id)">{{ scope.row.name }}</span>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-main>
+</template>
+
+<script>
+    import http from '../services/http';
+
+    export default {
+        data() {
+            return {
+                tableData: []
+            }
+        },
+        created() {
+            http.get('users', res => {
+                console.log(res);
+                this.tableData = res.data;
+            });
+        },
+        methods: {
+            toList(id) {
+                this.$router.push('users/' + id + '/expenses');
+            }
+        }
+    }
+</script>
+
+<style></style>
