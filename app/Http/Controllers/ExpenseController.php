7995d8015c685b3ac8535ca15dfd9a91185715d9
Norio Kaneko <akamt68@gmail.com>
2018-03-16 21:35:20 +0900

    自分以外のuser_idの時adminがあるかを判断・status codeの修正

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -8,30 +8,54 @@ use App\Expense;
 
 class ExpenseController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return mixed
-     */
-    public function getExpenseList(Request $request)
-    {
-        $user = \JWTAuth::parseToken()->authenticate();
-        $expense = new Expense();
+    protected $user;
 
-        return $expense->expenseList($user->id, $request->period);
+    /**
+     * ExpenseController constructor.
+     *
+     */
+    public function __construct()
+    {
+        $this->user = \JWTAuth::parseToken()->authenticate();
+    }
+
+    /**
+     * userIdを比較
+     * 権限に応じて返す
+     *
+     * @param integer $userId
+     * @return boolean
+     */
+    private function isAdmin($userId)
+    {
+        // 自分自身のID
+        $myUserId = $this->user->id;
+
+        if ($userId != $myUserId) {
+            if ($this->user->can('admin')) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
     }
 
     /**
      * 期間の一覧を返す
      *
      * @param integer $userId
-     * @return \Illuminate\Support\Collection
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Support\Collection
      */
     public function getPeriod($userId)
     {
-        $expense = new Expense();
-        return $expense->periodList($userId);
+        if ($this->isAdmin($userId)) {
+            $expense = new Expense();
+            return $expense->periodList($userId);
+        } else {
+            return response()->json(['message' => 'non admin'], 405);
+        }
     }
 
     /**
@@ -41,31 +65,35 @@ class ExpenseController extends Controller
      * @param $period
      * @return \Illuminate\Support\Collection
      */
-    public function getExpenses($userId, $period)
+    public function show($userId, $period)
     {
-        $expense = new Expense();
-        return $expense->getList($userId, $period);
+        if ($this->isAdmin($userId)) {
+            $expense = new Expense();
+            return $expense->getList($userId, $period);
+        } else {
+            return response()->json(['message' => 'non admin'], 405);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      *
+     * @param integer $userId
      * @param  \Illuminate\Http\Request $request
      * @return mixed
      */
-    public function store(Request $request)
+    public function store($userId, Request $request)
     {
-        $user = \JWTAuth::parseToken()->authenticate();
-        $array = $request->forms;
+        $expenses = $request->forms;
 
-        foreach ($array as $item) {
-            $item['user_id'] = $user->id;
+        foreach ($expenses as $item) {
+            $item['user_id'] = $userId;
             $item['use_day'] = Carbon::createFromFormat('Y-m-d', $item['date'])->format('Y-m-d');
             $item['month'] = Carbon::createFromFormat('Y-m-d', $item['date'])->format('Y-m');
             $expense = new Expense($item);
             $expense->save();
         }
-        return $user->id;
+        return response()->json('created!', 205);
     }
 
     /**
@@ -85,7 +113,7 @@ class ExpenseController extends Controller
             $expense->fill($value)->save();
         }
 
-        return response()->json('updated!', 200);
+        return response()->json('updated!', 201);
     }
 
     /**
