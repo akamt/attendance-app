59900959c765982cf0c1695acd91975f1836d3e7
Norio Kaneko <akamt68@gmail.com>
2018-03-16 21:24:05 +0900

    使用していない関数を削除

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,39 +11,12 @@ class UserController extends Controller
     /**
      * 部下の一覧を返す
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
         $roles = \JWTAuth::parseToken()->authenticate();
-
         $user = new User();
-
         return $user->getUserList($roles->role_id, $roles->group_id);
-    }
-
-    /**
-     * 登録されている経費の期間を取得
-     *
-     * @param $id
-     * @return \Illuminate\Support\Collection
-     */
-    public function getPeriodList($id)
-    {
-        $user = new User();
-        return $user->getPeriodList($id);
-    }
-
-    /**
-     * 経費一覧を返す
-     *
-     * @param $userId
-     * @param $period
-     * @return \Illuminate\Support\Collection
-     */
-    public function getExpenseList($userId, $period)
-    {
-        $user = new User();
-        return $user->getExpenseList($userId, $period);
     }
 }
