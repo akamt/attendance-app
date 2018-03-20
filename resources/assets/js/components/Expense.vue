18f15c0398a2f0f2049d4a530a4fe2a0ea5f6709
Norio Kaneko <akamt68@gmail.com>
2018-03-20 13:26:44 +0900

    経費登録画面のoverflow指定、変数定義

--- resources/assets/js/components/Expense.vue
+++ resources/assets/js/components/Expense.vue
@@ -1,6 +1,6 @@
 <template>
     <el-main v-loading="categoryLoading">
-        <el-header height="50px">
+        <el-header>
             <span>日付</span>
             <span>経費名</span>
             <span>金額</span>
@@ -8,38 +8,40 @@
             <span>種類</span>
         </el-header>
 
-        <el-form :model="form" :rules="rules" v-for="(form, index) in forms" :key="index"
-                 :ref="`dynamicFieldsForm${index}`">
-            <el-form-item prop="date">
-                <el-date-picker v-model="form.date" value-format="yyyy-MM-dd" type="date"
-                                placeholder="対象日を選択">
-                </el-date-picker>
-            </el-form-item>
-            <el-form-item prop="name">
-                <el-input placeholder="経費名" v-model="form.name" clearable>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="value">
-                <el-input v-model.number="form.value">
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="payment">
-                <el-input placeholder="支払先" v-model="form.payment" clearable>
-                </el-input>
-            </el-form-item>
-            <el-form-item prop="category">
-                <el-select v-model="form.category_id" placeholder="種類を選択">
-                    <el-option
-                            v-for="item in options"
-                            :key="item.id"
-                            :label="item.name"
-                            :value="item.id">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <i v-if="index >=1" class="el-icon-circle-close" @click="deleteForm(index)">
-            </i>
-        </el-form>
+        <div class="form-wrapper">
+            <el-form :model="form" :rules="rules" v-for="(form, index) in forms" :key="index"
+                     :ref="`dynamicFieldsForm${index}`">
+                <el-form-item prop="date">
+                    <el-date-picker v-model="form.date" value-format="yyyy-MM-dd" type="date"
+                                    placeholder="対象日を選択">
+                    </el-date-picker>
+                </el-form-item>
+                <el-form-item prop="name">
+                    <el-input placeholder="経費名" v-model="form.name" clearable>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="value">
+                    <el-input v-model.number="form.value">
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="payment">
+                    <el-input placeholder="支払先" v-model="form.payment" clearable>
+                    </el-input>
+                </el-form-item>
+                <el-form-item prop="category">
+                    <el-select v-model="form.category_id" placeholder="種類を選択">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.id"
+                                :label="item.name"
+                                :value="item.id">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <i v-if="index >=1" class="el-icon-circle-close" @click="deleteForm(index)">
+                </i>
+            </el-form>
+        </div>
 
         <div class="add-item-area">
             <el-button icon="el-icon-circle-plus" @click="addForm">
@@ -47,7 +49,7 @@
             </el-button>
         </div>
 
-        <div class="total-value-area">
+        <div class="register-area">
             <span>
                 合計金額
                 ¥
@@ -165,6 +167,11 @@
 </script>
 
 <style lang="scss">
+    $header-height: 60px;
+    $add-area-height: 40px;
+    $register-area-height: 50px;
+    $margin-height: 10px;
+
     .el-header {
         align-items: center;
         border-bottom: 1px solid #ebeef5;
@@ -172,7 +179,8 @@
         display: flex;
         font-size: 14px;
         font-weight: bold;
-        margin-bottom: 10px;
+        height: $header-height;
+        margin-bottom: $margin-height;
         position: relative;
 
         > span {
@@ -182,51 +190,67 @@
         }
     }
 
-    .el-form {
-        align-items: center;
-        display: flex;
-        margin-bottom: 20px;
-        position: relative;
+    .el-main {
+        height: 100%;
+        box-sizing: border-box;
 
-        .el-form-item {
+        .form-wrapper {
+            max-height: calc(100% - #{$header-height} - #{$add-area-height} - #{$register-area-height} - (#{$margin-height} * 2));
+            overflow: scroll;
+            &::-webkit-scrollbar {
+                display: none;
+            }
+
+            .el-form {
+                align-items: center;
+                display: flex;
+                margin-bottom: 20px;
+                position: relative;
+
+                .el-form-item {
+                    box-sizing: border-box;
+                    margin: 0;
+                    padding: 0 10px;
+                    width: calc(100% / 5);
+                    &:last-of-type {
+                        width: calc((100% / 5) - 16px);
+                    }
+
+                    .el-form-item__content {
+                        .el-select {
+                            width: 100%;
+                        }
+
+                        .el-date-editor--date {
+                            width: 100%;
+                        }
+                    }
+                }
+
+                .el-icon-circle-close {
+                    cursor: pointer;
+                    position: absolute;
+                    right: 0;
+                }
+            }
+        }
+
+        .add-item-area {
+            display: flex;
+            justify-content: center;
+            height: 40px;
+        }
+
+        .register-area {
+            align-items: center;
+            border-top: 1px solid #ebeef5;
             box-sizing: border-box;
-            margin: 0;
-            padding: 0 10px;
-            width: calc(100% / 5);
-            &:last-of-type {
-                width: calc((100% / 5) - 16px);
-            }
-
-            .el-form-item__content {
-                .el-select {
-                    width: 100%;
-                }
-
-                .el-date-editor--date {
-                    width: 100%;
-                }
-            }
+            color: #909399;
+            display: flex;
+            height: $register-area-height;
+            justify-content: space-around;
+            margin-top: $margin-height;
+            padding-top: 10px;
         }
-
-        .el-icon-circle-close {
-            cursor: pointer;
-            position: absolute;
-            right: 0;
-        }
-    }
-
-    .add-item-area {
-        display: flex;
-        justify-content: center;
-    }
-
-    .total-value-area {
-        align-items: center;
-        border-top: 1px solid #ebeef5;
-        color: #909399;
-        display: flex;
-        justify-content: space-around;
-        margin: 10px 0;
-        padding: 10px 0;
     }
 </style>
