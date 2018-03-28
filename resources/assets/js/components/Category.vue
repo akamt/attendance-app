0a105ce963d92c277d4d4c2cf0d0525a99b522a2
Norio Kaneko <akamt68@gmail.com>
2018-03-28 21:45:56 +0900

    カテゴリー追加画面のvalidate追加

--- resources/assets/js/components/Category.vue
+++ resources/assets/js/components/Category.vue
@@ -1,12 +1,14 @@
 <template>
     <el-main>
-        <el-form @keyup.enter.native="login" ref="form" :model="form" label-width="100px">
-            <el-form-item label="項目名">
+        <el-form ref="categoryForm" :model="form" label-width="100px">
+            <el-form-item label="項目名"
+                          prop="name"
+                          :rules="{required: true, message: 'カテゴリー名を入力してください', trigger: 'blur'}">
                 <el-input placeholder="経費項目名" v-model="form.name" clearable>
                 </el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="add">Add</el-button>
+                <el-button type="primary" @click="validate">Add</el-button>
             </el-form-item>
         </el-form>
     </el-main>
@@ -18,19 +20,30 @@
     export default {
         data() {
             return {
-                form:{
-                    name:''
+                form: {
+                    name: ''
                 }
             }
         },
         methods: {
+            validate() {
+                this.$refs['categoryForm'].validate((valid) => {
+                    if (valid) {
+                        this.add();
+                    }
+                });
+            },
             add() {
                 let data = {
-                    name: this.name
+                    name: this.form.name
                 };
 
                 http.post('categories', data, res => {
-                    console.log(res);
+                    this.$message({
+                        message: '保存しました',
+                        type: 'success'
+                    });
+                    this.form.name = '';
                 })
             }
         }
